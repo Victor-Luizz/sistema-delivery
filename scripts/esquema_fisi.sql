c9180e82571276612eb8544add59b7e87c1d6b9d
Victor Luiz <victorluizribeirodasilva@gmail.com>
2026-09-08 23:22:30 -0300

    Primeiro commit:modelo e scripts do sistema delivery

---
+++ scripts/esquema_fisi.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: reserva
+-- ------------------------------------------------------
+-- Server version	9.6.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ 'b0375c9c-11d4-11f1-9ce4-00d861077f4b:1-60';
+
+--
+-- Dumping data for table `cardapio`
+--
+
+LOCK TABLES `cardapio` WRITE;
+/*!40000 ALTER TABLE `cardapio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cardapio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `contem`
+--
+
+LOCK TABLES `contem` WRITE;
+/*!40000 ALTER TABLE `contem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `entregador`
+--
+
+LOCK TABLES `entregador` WRITE;
+/*!40000 ALTER TABLE `entregador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entregador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `fazer`
+--
+
+LOCK TABLES `fazer` WRITE;
+/*!40000 ALTER TABLE `fazer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fazer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `restaurante`
+--
+
+LOCK TABLES `restaurante` WRITE;
+/*!40000 ALTER TABLE `restaurante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `telefone_clien`
+--
+
+LOCK TABLES `telefone_clien` WRITE;
+/*!40000 ALTER TABLE `telefone_clien` DISABLE KEYS */;
+/*!40000 ALTER TABLE `telefone_clien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `telefone_entreg`
+--
+
+LOCK TABLES `telefone_entreg` WRITE;
+/*!40000 ALTER TABLE `telefone_entreg` DISABLE KEYS */;
+/*!40000 ALTER TABLE `telefone_entreg` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-08 23:13:18
